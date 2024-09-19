1f86eb3ccc53da026caf12e52ba6f3ed5d343668
sandboxSamuel <134462089+sandboxSamuel@users.noreply.github.com>
2024-09-18 23:10:51 -0400

    added iteration to images

--- site/interactivitywithjs-UniversityofMichigan/js/gallery.js
+++ site/interactivitywithjs-UniversityofMichigan/js/gallery.js
@@ -1,18 +1,42 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){
-    x = previewPic.getAttribute("src");
-    y = previewPic.getAttribute("alt");
-  
-    document.getElementById('image').style.backgroundImage = "url('" + x + "')";
-    document.getElementById('image').innerHTML = y;
-    console.log(x);
-    console.log(y);
-    
-      }
-  
-      function unDo(){
-      x=document.getElementById("image");
-      x.style.backgroundImage="url('')";
-      document.getElementById('image').innerHTML = "Hover over an image below to display here.";
-    }
+function upDate(previewPic) {
+  x = previewPic.getAttribute("src");
+  y = previewPic.getAttribute("alt");
+
+  document.getElementById("image").style.backgroundImage = "url('" + x + "')";
+  document.getElementById("image").innerHTML = y;
+  console.log(x);
+  console.log(y);
+}
+
+function unDo() {
+  x = document.getElementById("image");
+  x.style.backgroundImage = "url('')";
+  document.getElementById("image").innerHTML =
+    "Hover over an image below to display here.";
+}
+
+function upDateTab(previewPic) {
+  x = previewPic.getAttribute("src");
+  y = previewPic.getAttribute("alt");
+
+  document.getElementById("image").style.backgroundImage = "url('" + x + "')";
+  document.getElementById("image").innerHTML = y;
+  console.log(x);
+  console.log(y);
+}
+
+function unDoTab() {
+  x = document.getElementById("image");
+  x.style.backgroundImage = "url('')";
+  document.getElementById("image").innerHTML =
+    "Hover over an image below to display here.";
+}
+
+updateImages = document.querySelectorAll("img");
+for (let i = 0; i < updateImages.length; i++) {
+  updateImages[i].setAttribute("tabindex", "0");
+  updateImages[i].setAttribute("onfocus", "upDateTab(this)");
+  updateImages[i].setAttribute("onblur", "unDoTab(this)");
+}
